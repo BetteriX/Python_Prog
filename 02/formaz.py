df0c7456f5763fe2250220020e8b9d7730f60777
BetteriX <szabozsolt430@gmail.com>
2024-09-16 11:33:54 +0200

    2_ora

---
+++ 02/formaz.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+
+MAX = 100
+
+import sys
+
+def hello(name, color, what):
+    #v1
+    #print("{0}, {1} az {2}! Ki? {0}".format(name, color, what))
+    
+    #v2 #Hibás
+    #print("{}, {} az {}! Ki? {}".format(name, color, what))
+    
+    #v3
+    print("{n}, {c} az {w}!".format(n=name.capitalize(), c=color, w=what))
+    
+    #v4
+    #print(f"{name}, {color} az {what}!")
+
+def main():
+    hello("geza", "kek", "eg")
+    print("-" * 40)
+
+if __name__ == "__main__":
+    main()
