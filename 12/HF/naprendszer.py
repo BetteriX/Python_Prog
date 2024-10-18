6270767ee01785707b2dcb7edb56cd82fcf852e1
BetteriX <szabozsolt430@gmail.com>
2024-10-18 10:43:31 +0200

    naprendszerek

---
+++ 12/HF/naprendszer.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+
+
+def sorrend_ellenoriz(szoveg):
+    szavak = ["j", "s", "u", "n"]
+    i = 0
+    for c in szoveg:
+        if c == szavak[i]:
+            i += 1
+
+    if i == 4:
+        return True
+
+    return False
+
+
+def main():
+    f = open("DT2.csv", "r")
+    szavak = []
+
+    for line in f:
+        line = line.split(",")
+        szoveg = line[0]
+
+        if sorrend_ellenoriz(szoveg):
+            szavak.append(szoveg)
+
+    f.close()
+
+    print(szavak)
+
+
+if __name__ == "__main__":
+    main()
